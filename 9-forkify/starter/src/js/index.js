b64d0fe78acbd89a9c41a59410cde4bd74dd6ed0
Aleksandr Kravchenko <a.kravchenko.intek@gmail.com>
2020-03-21 11:53:18 +0100

    search input, search list

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,39 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`An imported ${num} from another module. Variable x is: ${x}`);
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
+
+/**
+ * Global state of out app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+  
+  // 1. Get query from view
+  const query = searchView.getInput();
+
+  if (query) {
+    // 2. New search object and add to state
+    state.search = new Search(query);
+
+    // 3. Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+
+    // 4. Search for recipes
+    await state.search.getResult();
+
+    // 5. Render result on UI
+    searchView.renderResults(state.search.result);
+  }
+  // 2. 
+};
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
